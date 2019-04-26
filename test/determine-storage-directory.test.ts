67ecbb5e718dc3743e5c9bc7a388d571337cbb6e
👨🏼‍💻 Romain Marcadier-Muller <romain.muller@telecomnancy.net>
2019-04-26 13:04:37 +0200

    Fix test

--- test/determine-storage-directory.test.ts
+++ test/determine-storage-directory.test.ts
@@ -1,10 +1,11 @@
 import fs = require('fs-extra');
 import os = require('os');
+import path = require('path');
 import { determineStorageDirectory } from '../lib/determine-storage-directory';
 
 describe('determineStorageDirectory', () => {
   test('when $PKO_REPOSITORY is defined', async () => {
-    const expected = await fs.mkdtemp(os.tmpdir());
+    const expected = await fs.mkdtemp(path.join(os.tmpdir(), 'determineStorageDirectory'));
     try {
       process.env.PKO_REPOSITORY = expected;
       return await expect(determineStorageDirectory()).resolves.toBe(expected);
